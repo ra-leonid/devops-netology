dea925307da6adea9bde022d244a846baa159056
Leonid Rastorguev <rastorguyevl@list.ru>
2023-02-25 19:37:47 +0300

    ДЗ 15-clokub-03-security

---
+++ homeworks/15-clokub-03-security/terraform/provider.tf
@@ -0,0 +1,19 @@
+# Provider
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      //version = "0.61.0"
+      version = "0.85.0"
+    }
+  }
+}
+
+provider "yandex" {
+  # yc_token, yc_folder_id, yc_cloud_id хранятся локально в файле terraform.tfvars
+  # Для использования переменных, объявляем их в файле variables.tf, но не заполняем
+  token     = var.yc_token
+  folder_id = var.yc_folder_id
+  cloud_id  = var.yc_cloud_id
+  zone      = var.yc_region
+}
